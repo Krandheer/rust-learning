670e38344f8817b1219ca44238d95304b1f74e16
randheer <randheer.dakshana15@gmail.com>
2024-02-01 18:32:57 +0530

    struct done

---
+++ src/struct_rust.rs
@@ -0,0 +1,16 @@
+struct ShippingBox {
+    height: i32,
+    width: i32,
+    depth: i32,
+}
+
+fn main() {
+    let my_box = ShippingBox {
+        height: 3,
+        width: 4,
+        depth: 5,
+    };
+
+    let tall = my_box.height;
+    println!("{:?}", tall);
+}
