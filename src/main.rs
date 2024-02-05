91feaf6e9055c9a9f6496d6ccb4e01aeeb430398
randheer <randheer.dakshana15@gmail.com>
2024-02-05 15:50:22 +0530

    rand method to get random input from users

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use std::io;
-
-fn main() {
-    let vec: Vec<i32> = vec![];
-    let num = [1, 2, 3];
-    println!("{:?}", num);
-
-    if vec.is_empty() {
-        println!("true");
-    }
+// fn main() {
+//     let vec: Vec<i32> = vec![];
+//     let num = [1, 2, 3];
+//     println!("{:?}", num);
+//
+//     if vec.is_empty() {
+//         println!("true");
+//     }
 
     // println!("Guess the number!");
     //
@@ -24,4 +22,19 @@ fn main() {
     // let x = 4;
     // println!("{}", x);
     // println!("{}", x);
+// }
+
+use rand::Rng;
+use std::io;
+
+fn main() {
+    println!("gues the number!");
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+    println!("plz input your guess");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+    println!("your guessed number is {guess}");
+    println!("the secret number is: {secret_num}");
 }
