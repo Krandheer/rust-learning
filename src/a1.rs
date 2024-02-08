de5d2477e15b0b7ab8a5c2769303993638cb58bc
randheer <randheer.dakshana15@gmail.com>
2024-02-09 01:37:46 +0530

    checking float division

--- src/a1.rs
+++ src/a1.rs
@@ -1,16 +1,15 @@
-fn first_name() {
-    print!("randheer ");
-}
-fn last_name() {
-    println!("gautam");
-}
-/// .
-// pub(crate) fn complete_name() {
-//     first_name();
-//     last_name();
+// fn first_name() {
+//     print!("randheer ");
+// }
+// fn last_name() {
+//     println!("gautam");
 // }
 
-fn main() {
-    first_name();
-    last_name();
+pub(crate) fn complete_name() {
+    // first_name();
+    // last_name();
+    let a = 2.0;
+    let b = 7.0;
+    let c = b / a;
+    println!("c is : {c}");
 }
