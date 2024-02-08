f76b981172617046aad319575a1be6d8d5cae913
randheer <randheer.dakshana15@gmail.com>
2024-02-08 11:16:09 +0530

    guessing the number game completes now

--- src/main.rs
+++ src/main.rs
@@ -38,12 +38,15 @@ fn main() {
             .read_line(&mut guess)
             .expect("failed to read line");
         println!("your guessed number is {guess}");
-        println!("the secret number is: {secret_num}");
+        // println!("the secret number is: {secret_num}");
 
-        let guess: i32 = guess.trim().parse().expect("please type a number");
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("\nusing ordering function:");
-        match secret_num.cmp(&guess) {
+        match guess.cmp(&secret_num) {
             Ordering::Less => println!("your number is less"),
             Ordering::Greater => println!("your number is more than secret number"),
             Ordering::Equal => {
