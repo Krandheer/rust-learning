b2933f972d8726b783532fcb69669f0d09a18eea
randheer <randheer.dakshana15@gmail.com>
2024-02-08 21:49:46 +0530

    exploring variables in rust

---
+++ src/variables.rs
@@ -0,0 +1,15 @@
+pub(crate) fn learn_variables() {
+    // let mut x = 5;
+    // println!("the value of x is: {x}");
+    // x = 6;
+    // println!("the value of x is: {x}");
+
+    //scoping and shadoing of variable
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("the value of x in inner scope is {x}");
+    }
+    println!("value of x is: {x}")
+}
