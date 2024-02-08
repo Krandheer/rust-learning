981bb861c450e674e9925baad51382b3d579d5c8
randheer <randheer.dakshana15@gmail.com>
2024-02-08 11:01:30 +0530

    guessing the number game done

--- src/main.rs
+++ src/main.rs
@@ -31,26 +31,32 @@ use std::io;
 fn main() {
     println!("guess the number!");
     let secret_num = rand::thread_rng().gen_range(1..=100);
-    println!("plz input your guess");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
-    println!("your guessed number is {guess}");
-    println!("the secret number is: {secret_num}");
+    loop {
+        println!("plz input your guess");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+        println!("your guessed number is {guess}");
+        println!("the secret number is: {secret_num}");
 
-    let guess: i32 = guess.trim().parse().expect("please type a number");
+        let guess: i32 = guess.trim().parse().expect("please type a number");
 
-    println!("\nusing ordering function:");
-    match secret_num.cmp(&guess) {
-        Ordering::Less => println!("your number is less"),
-        Ordering::Greater => println!("your number is less"),
-        Ordering::Equal => println!("your number is less"),
-    }
-    println!("\nusing else if:");
-    if guess == secret_num {
-        println!("your guess is right");
-    } else {
-        println! {"your guess is wrong"}
+        println!("\nusing ordering function:");
+        match secret_num.cmp(&guess) {
+            Ordering::Less => println!("your number is less"),
+            Ordering::Greater => println!("your number is more than secret number"),
+            Ordering::Equal => {
+                println!("your number is correct");
+                break;
+            }
+        }
+        println!("\nusing else if:");
+        if guess == secret_num {
+            println!("your guess is right");
+            break;
+        } else {
+            println! {"your guess is wrong"}
+        }
     }
 }
