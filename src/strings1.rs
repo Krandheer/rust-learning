12187c095d8e869e36e33508edc331f82ff66890
randheer <randheer.dakshana15@gmail.com>
2024-02-09 16:07:52 +0530

    experimenting with ownership on basis of documentation

--- src/strings1.rs
+++ src/strings1.rs
@@ -9,6 +9,7 @@ struct LineItem {
     count: i32,
 }
 fn some_integer(mut some_integer: i32) {
+    // now here some_integer is passed and can be used till when it does not goes out of scope
     println!("some integers: {some_integer}");
     some_integer = some_integer + 1;
     println!("some integers: {some_integer}");
@@ -43,15 +44,10 @@ fn main() {
     println!("");
     let mut s = String::from("hello");
     s.push_str(", world");
-    // println!("s: {s}, s_len={Len(s)}");
-    let mut x = 5;
-    some_strings(s);
-    // some_integer(x);
-    println!("x: {x}");
-
-    // this below loop throws error because receipt vector got used once and hence doesn't exist in
-    // memeory
-    // for item in receipt {
-    //     println!("name: {:?} and number: {:?}", item.name, item.count);
-    // }
+    println!("s: {s}, ");
+    let p = &s;
+    let q = &s;
+    println!("this is p: {p} and this is q: {q}");
+    let r = &mut s; // after declaring mutable variable to r the scope of p and q ends; 
+    println!("r is r: {r}");
 }
