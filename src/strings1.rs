b735800f36199b1c725d7ed50122b25a4569007a
randheer <randheer.dakshana15@gmail.com>
2024-02-10 14:59:43 +0530

    string slices done

--- src/strings1.rs
+++ src/strings1.rs
@@ -18,36 +18,51 @@ fn some_integer(mut some_integer: i32) {
 fn some_strings(some_string: String) {
     println!("some string: {some_string}");
 }
+
+fn slices(word: &str) -> &str {
+    let bytes = word.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &word[0..i];
+        }
+    }
+    return &word[..];
+}
 fn main() {
-    let receipt = vec![
-        LineItem {
-            name: "cereal".to_owned(),
-            count: 21,
-        },
-        LineItem {
-            name: "fruits".to_owned(),
-            count: 11,
-        },
-        LineItem {
-            name: "vegetables".to_owned(),
-            count: 31,
-        },
-        LineItem {
-            name: "potatoes".to_owned(),
-            count: 61,
-        },
-    ];
+    // let receipt = vec![
+    //     LineItem {
+    //         name: "cereal".to_owned(),
+    //         count: 21,
+    //     },
+    //     LineItem {
+    //         name: "fruits".to_owned(),
+    //         count: 11,
+    //     },
+    //     LineItem {
+    //         name: "vegetables".to_owned(),
+    //         count: 31,
+    //     },
+    //     LineItem {
+    //         name: "potatoes".to_owned(),
+    //         count: 61,
+    //     },
+    // ];
     // for item in receipt {
     //     println!("name: {:?} and number: {:?}", item.name, item.count);
     // }
 
-    println!("");
-    let mut s = String::from("hello");
-    s.push_str(", world");
-    println!("s: {s}, ");
-    let p = &s;
-    let q = &s;
-    println!("this is p: {p} and this is q: {q}");
-    let r = &mut s; // after declaring mutable variable to r the scope of p and q ends; 
-    println!("r is r: {r}");
+    // println!("");
+    // let mut s = String::from("hello");
+    // s.push_str(", world");
+    // println!("s: {s}, ");
+    // let p = &s;
+    // let q = &s;
+    // println!("this is p: {p} and this is q: {q}");
+    // let r = &mut s; // after declaring mutable variable to r the scope of p and q ends;
+    // println!("r is r: {r}");
+    let mut s = String::from("hello world");
+    let word = slices(&s); // here word is reference to slice hello
+    // we can't do mutable operation on s and then use referce to s through word in println.
+    // s.clear();
+    println!("word is: {word}");
 }
