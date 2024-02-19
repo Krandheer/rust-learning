916142d65a2db41b2060758352a970245208a16a
randheer <randheer.dakshana15@gmail.com>
2024-02-19 13:13:26 +0530

    comments added

--- src/options.rs
+++ src/options.rs
@@ -6,7 +6,6 @@ fn plus_one(x: Option<i32>) -> Option<i32> {
 }
 
 fn main() {
-
     // Option<T> have two enums inside it, one is Some of type T and another is None
     let five = Some(5); // this is and enum with int as Type
     let six = plus_one(five);
