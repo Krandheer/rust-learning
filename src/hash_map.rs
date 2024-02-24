a084652fd0be4fc97676b96a76588ad38100420c
randheer <randheer.dakshana15@gmail.com>
2024-02-24 10:40:17 +0530

    hash-map learning

--- src/hash_map.rs
+++ src/hash_map.rs
@@ -8,4 +8,22 @@ fn main() {
     for (key, val) in hash_map.iter() {
         println!("key: {:?}, value: {:?}", key, val);
     }
+    for (key, val) in &hash_map {
+        println!("{key}: {val}");
+    }
+    println!("{:?}", hash_map);
+
+    println!("");
+    use std::collections::HashMap;
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0); // not able to understand this line
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
