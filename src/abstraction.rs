5d7bbfe0ced39bbf6500a4a0280d11b909bdb8d7
randheer <randheer.dakshana15@gmail.com>
2024-02-25 13:15:29 +0530

    generic function

--- src/abstraction.rs
+++ src/abstraction.rs
@@ -10,10 +10,23 @@ fn largest(list: &[i32]) -> i32 {
     *largest
 }
 
+
+// generice function that can be used in case of number and characters for same work
+fn largest2<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
+    let result = largest2(&number_list);
     println!("The largest number is {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
